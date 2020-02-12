4bbf6053b3155bc8e7a19428a325aab07c66114c
pawel.ciupka <pawel.ciupka@accenture.com>
2020-02-12 02:38:14 -0800

    Complete chapter "2. Display a List"

---
+++ angular-tour-of-heroes/src/app/mock-heroes.ts
@@ -0,0 +1,14 @@
+import { Hero } from './hero';
+
+export const HEROES: Hero[] = [
+  { id: 1, name: 'Pan Paweł' },
+  { id: 2, name: 'Pan Janusz' },
+  { id: 3, name: 'Pani Olito' },
+  { id: 4, name: 'Transformers' },
+  { id: 5, name: 'JavaScript' },
+  { id: 6, name: 'HTML' },
+  { id: 7, name: 'SCSS' },
+  { id: 8, name: 'Node.js' },
+  { id: 9, name: 'XD' },
+  { id: 10, name: 'Nowy Jorek' }
+];
